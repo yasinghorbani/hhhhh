284faba1f024e419910a8899ffef6aec03ba6255
Armin <armin.bagheri.1380@gmail.com>
2016-04-17 19:44:19 +0430

    Update helpme.lua

--- plugins/helpme.lua
+++ plugins/helpme.lua
@@ -1,18 +1,116 @@
-do
-function run(msg, matches)
-  return "💠TeleIran Tools Commands: \n\n\n 🚫Anti Words: \n #block [Word] \n #unblock [Word] \n\n ❌Anti Tag: \n #block @ \n #block # \n\n  ❌Anti Link: \n #lock links \n #block telegram.me \n\n ❌Anti Emoji: \n #block [Emoji] \n\n ❌Anti Sticker: \n #lock sticker \n\n 🎁Sticker Maker: \n #sticker \n #top \n\n 📷WebShot: \n #web [Site] \n\n 📚Calculator: \n #calc [Number] \n\n 🎤Text To Voice: \n #vc [Text] \n\n 🌦Weather: \n #weather [City] \n\n ☢About TeleIan: \n #TeleIran \n\n 🚀 Channel: @TeleIranTeam 💎"
+
+local function run(msg, matches)
+  if is_chat_msg(msg) then
+    local text = [[
+💠TeleIran Tools Commands: 
+
+
+ 🚫Anti Words: 
+ #block [Word] 
+ #unblock [Word] 
+
+ ❌Anti Tag: 
+ #block @ 
+ #block # 
+
+  ❌Anti Link: 
+ #lock links 
+ #block telegram.me 
+
+ ❌Anti Emoji: 
+ #block [Emoji] 
+
+ ❌Anti Sticker: 
+ #lock sticker 
+
+ 🎁Sticker Maker: 
+ #sticker 
+ #top 
+
+ 📷WebShot: 
+ #web [Site] 
+
+ 📚Calculator: 
+ #calc [Number] 
+
+ 🎤Text To Voice: 
+ #vc [Text] 
+
+ 🌦Weather: 
+ #weather [City] 
+
+ ☢About TeleIan: 
+ #TeleIran 
+
+ 🚀 Channel: @TeleIranTeam 💎
+]]
+    return text
   end
+  if is_channel_msg(msg) then
+    local text = [[
+💠TeleIran Tools Commands: 
+
+
+ 🚫Anti Words: 
+ #block [Word] 
+ #unblock [Word] 
+
+ ❌Anti Tag: 
+ #block @ 
+ #block # 
+
+  ❌Anti Link: 
+ #lock links 
+ #block telegram.me 
+
+ ❌Anti Emoji: 
+ #block [Emoji] 
+
+ ❌Anti Sticker: 
+ #lock sticker 
+
+ 🎁Sticker Maker: 
+ #sticker 
+ #top 
+
+ 📷WebShot: 
+ #web [Site] 
+
+ 📚Calculator: 
+ #calc [Number] 
+
+ 🎤Text To Voice: 
+ #vc [Text] 
+
+ 🌦Weather: 
+ #weather [City] 
+
+ ☢About TeleIan: 
+ #TeleIran 
+
+
+ 🚀 Channel: @TeleIranTeam 💎
+]]
+    return text
+  else
+    local text = [[aaa]]
+    --return text
+  end
+end
+
 return {
-  description = " TeleIran ", 
-  usage = " TeleIran ",
+  description = " Help Plugin ", 
+  usage = {
+    " /help ",
+  },
   patterns = {
-    "^[!#/][Hh][Ee][Ll][Pp]",
+    "^[#!/](help)$",
+    "^[!#/][Hh][Ee][Ll][Pp]$",
     "^[#/!][Hh][Ee][Ll][Pp][Mm][Ee]$",
     "^[#/!][Hh][Ee][Ll][Pp] [Mm][Ee]$",
     "^[#/!][Hh][Ee][Ll][Pp][Aa][Ll][Ll]$",
     "^[#/!][Hh][Ee][Ll][Pp] [Aa][Ll][Ll]$",
     "^[#/!][Ss][Hh][Ee][Ll][Pp]$",
-  },
-  run = run
+  }, 
+  run = run,
 }
-end
