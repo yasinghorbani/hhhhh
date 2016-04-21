bafe8dcf25287d8d247d863ef4418fc1c330e0f9
Armin <armin.bagheri.1380@gmail.com>
2016-04-21 21:53:27 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,88 +1,18 @@
-local function run(msg, matches)
-  if is_chat_msg(msg) then
-    local text = [[
-💠TeleIran Tools Commands: 
-
-
- 🚫Anti Words: 
- #block [Word] 
- #unblock [Word] 
-
- 🎁Sticker Maker: 
- #sticker 
- #png
-
- 📷WebShot: 
- #web [Site] 
-
- 📚Calculator: 
- #calc [Number] 
-
- 🎤Text To Voice: 
- #vc [Text] 
-
- 🌦Weather: 
- #weather [City] 
-
- ☢About TeleIan: 
- #TeleIran 
-
-
- 🚀 Channel: @TeleIranTeam 💎
-]]
-    return text
+do
+function run(msg, matches)
+  return " 💠TeleIran Tools Commands: \n\n\n 🚫Anti Words: \n #block [Word] \n #unblock [Word] \n\n 🎁Sticker Maker: \n #sticker \n #png \n\n 📷WebShot: \n #web [Site] \n\n 📚Calculator: \n #calc [Number] \n\n 🎤Text To Voice: \n #vc [Text] \n\n 🌦Weather: \n #weather [City] \n\n ⏰Online Clock: \n #time \n\n☢About TeleIan: \n #TeleIran \n\n\n 🚀 Channel: @TeleIranTeam 💎 "
   end
-  if is_channel_msg(msg) then
-    local text = [[
-💠TeleIran Tools Commands: 
-
-
- 🚫Anti Words: 
- #block [Word] 
- #unblock [Word] 
-
- 🎁Sticker Maker: 
- #sticker 
- #png
-
- 📷WebShot: 
- #web [Site] 
-
- 📚Calculator: 
- #calc [Number] 
-
- 🎤Text To Voice: 
- #vc [Text] 
-
- 🌦Weather: 
- #weather [City] 
-
- ☢About TeleIan: 
- #TeleIran 
-
-
- 🚀 Channel: @TeleIranTeam 💎
-]]
-    return text
-  else
-    local text = [[aaa]]
-    --return text
-  end
-end
-
 return {
-  description = " Help Plugin ", 
-  usage = {
-    " /help ",
-  },
+  description = " TeleIran ", 
+  usage = " TeleIran ",
   patterns = {
-    "^[#!/](help)$",
     "^[!#/][Hh][Ee][Ll][Pp]$",
     "^[#/!][Hh][Ee][Ll][Pp][Mm][Ee]$",
     "^[#/!][Hh][Ee][Ll][Pp] [Mm][Ee]$",
     "^[#/!][Hh][Ee][Ll][Pp][Aa][Ll][Ll]$",
     "^[#/!][Hh][Ee][Ll][Pp] [Aa][Ll][Ll]$",
     "^[#/!][Ss][Hh][Ee][Ll][Pp]$",
-  }, 
-  run = run,
+  },
+  run = run
 }
+end
