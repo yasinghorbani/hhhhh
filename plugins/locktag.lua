92854593edb708c1ec51e58758d227e42e183565
Armin <armin.bagheri.1380@gmail.com>
2016-04-21 11:02:52 +0430

    Update locktag.lua

--- plugins/locktag.lua
+++ plugins/locktag.lua
@@ -2,6 +2,8 @@ do
 local function run(msg, matches)
  if not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then 
 delete_msg(msg.id, ok_cb, false)
+send_report(msg)
+end
 end       
 end
 return {
