ee9c110e4ae9c7b9638b3192410430891258b9ed
Armin <armin.bagheri.1380@gmail.com>
2016-04-21 10:42:10 +0430

    Update rem.lua

--- plugins/rem.lua
+++ plugins/rem.lua
@@ -3,23 +3,23 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" Msg Is Removed', ok_cb, false)
   else
-    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
+    send_msg(extra.chatid, 'Massages Has Been Removed', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] == 'rmsg' and is_owner(msg) then
     if msg.to.type == 'channel' then
       if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
-        return "تعداد بیشتر از 1 مجاز است"
+        return "Only Remove +5 Massage"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "فقط در سوپرگروه ممکن است"
+      return "Only Work In SuperGroup"
     end
   else
-    return "شما دسترسی ندارید"
+    return "You Are Not Admin"
   end
 end
 
