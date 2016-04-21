4580a36ff85f7db0bab8d8c7161cafcb4d57265a
Armin <armin.bagheri.1380@gmail.com>
2016-04-21 21:37:45 +0430

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -1,37 +1,18 @@
 do
-
 local function run(msg, matches)
-
-
-local text = matches[1]
-
-  local b = 1
-
-  while b ~= 0 do
-    textc = text:trim()
-    text,b = text:gsub(' ','.')
-    
-    
-  if msg.to.type == 'user' then 
-      return nil
-      else
-  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
+  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..matches[1]
   local receiver = get_receiver(msg)
   local file = download_to_file(url,'TeleIran.ogg')
-      send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+      send_audio('channel#id'..msg.to.id, file, ok_cb , false)
 end
-end
-  end
+
 return {
   description = "text to voice",
   usage = {
-    "!vc [text]"
+    "!voice [text]"
   },
   patterns = {
-    "^!vc +(.*)$",
-    "^/vc +(.*)$",
-	  "^#vc +(.*)$",
-
+    "^[!/#]vc +(.+)$"
   },
   run = run
 }
