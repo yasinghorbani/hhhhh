e7834f495ab185c29f5bfd418f90aa8debbcf3e8
Armin <armin.bagheri.1380@gmail.com>
2016-04-28 16:53:18 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -13,7 +13,7 @@ update() {
 install_luarocks() {
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
-  git checkout tags/v2.3.0-rc2 # Release Candidate
+  git checkout tags/v2.2.1 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
 
@@ -33,27 +33,12 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasec
+  ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install lbase64 20120807-3
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luafilesystem
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lub
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luaexpat
+  ./.luarocks/bin/luarocks install oauth
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -107,16 +92,6 @@ install() {
   cd ..
   install_luarocks
   install_rocks
-  
-  if [ -d /mnt/c/Windows ]; then
-    echo "Patching bot.lua for Windows 10 support..."
-    sed -i '5d' bot/bot.lua
-    sed -i '5d' bot/bot.lua
-    sed -i '5i\require("bot/utils")' bot/bot.lua
-    sed -i '6i\require("bot/permissions")' bot/bot.lua
-    sed -i '7i\--File patched to support W10' bot/bot.lua
-  fi
-  
 }
 
 if [ "$1" = "install" ]; then
@@ -135,10 +110,9 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  
-
-  if [ -f data/config.lua ]; then
-    ./config_fix.sh
-  fi
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+   sleep 3
+  done
 fi
