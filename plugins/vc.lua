ca2298ccb58278d2725220eb859d914081896c54
Armin <armin.bagheri.1380@gmail.com>
2016-04-30 16:41:28 +0430

    Create vc.lua

---
+++ plugins/vc.lua
@@ -0,0 +1,20 @@
+local function run(msg, matches)
+  local eq = matches[1]
+
+  local url = "http://www.farsireader.com/PlayText.aspx?Text="..URL.escape(eq).."&Punc=false"
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url,'text.ogg')
+  send_audio('channel#id'..msg.to.id, file, ok_cb , false)
+send_audio('chat#id'..msg.to.id, file, ok_cb , false)
+end
+
+return {
+  description = "Convert text to voice",
+  usage = {
+    "/vc [text]: Convert text to voice"
+  },
+  patterns = {
+    "[!/#][Vv][Cc] (.+)$"
+  },
+  run = run
+}
