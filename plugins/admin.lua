b02151a430165ad366477820f1075ea2978b0b3f
Armin <armin.bagheri.1380@gmail.com>
2016-04-30 17:09:53 +0430

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, " TeleIran Is Reload ", ok_cb, false)
+		return " TeleIran Is Reload "
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
