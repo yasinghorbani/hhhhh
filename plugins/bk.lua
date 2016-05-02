d24f7efa3d22e66ecf61ff4a2b449fddf455b855
Armin <armin.bagheri.1380@gmail.com>
2016-05-02 15:50:16 +0430

    Update bk.lua

--- plugins/bk.lua
+++ plugins/bk.lua
@@ -2,8 +2,8 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = ' به کیرم آقا به کیییرم '
-if matches[1] == 'bk' then
+local text = 'bk'
+if matches[1] == ' به کیرم آقا به کیییرم ' then
     if (msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
