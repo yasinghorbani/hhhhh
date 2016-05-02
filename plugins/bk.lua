fa8b26cadd23952f878fb5d592fd31005b122830
Armin <armin.bagheri.1380@gmail.com>
2016-05-02 15:53:18 +0430

    Update bk.lua

--- plugins/bk.lua
+++ plugins/bk.lua
@@ -2,9 +2,8 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'bk'
-if matches[1] == ' به کیرم آقا به کیییرم ' then
-    if (msg) then
+local text = ' منظور شما از کلمه #bk یعنی به کیرم است '
+if matches[1] == 'bk' then
 reply_msg(reply_id, text, ok_cb, false)
 end
 end 
